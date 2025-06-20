12b3bf9574e9a63dfebbf727a5c5143287d9ab7e
ALONE-VIBER <kuttypulisurya22@gmail.com>
2025-06-20 07:37:36 +0530

    added header and menus

---
+++ home.js
@@ -0,0 +1,18 @@
+const min_menu_btn = document.querySelector("#min-menu-btn");
+const min_menu = document.querySelector("#min-menu");
+
+// Toggle the menu when the button is clicked
+min_menu_btn.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent it from triggering the outside click
+    min_menu_btn.classList.toggle("min-menu-active");
+    min_menu.classList.toggle("min-menu-active");
+});
+
+// Hide menu when clicking outside of it
+document.addEventListener("click", (event) => {
+    // Check if click is outside the min-menu and min-menu-btn
+    if (!min_menu.contains(event.target) ) {
+        min_menu.classList.remove("min-menu-active");
+        min_menu_btn.classList.remove("min-menu-active");
+    }
+});
