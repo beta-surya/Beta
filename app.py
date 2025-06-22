1075e263cfd8866ea964e2d3deab807b1f5d908b
ALONE-VIBER <kuttypulisurya22@gmail.com>
2025-06-22 22:26:10 +0530

    developed the backend for todo menu

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template , jsonify
+from flask import Flask, render_template , jsonify ,request
+from modules import todo
 
 app = Flask(__name__,
             template_folder='templates',
@@ -8,15 +9,37 @@ app = Flask(__name__,
 def home():
     return render_template("home.html")
 
-@app.route("/todo")
-def getTodo():
-    return jsonify({"msg": "This is the TODO response"})
+@app.route('/gettodo')
+def sendTodo():
+    data = todo.get_data()
+    if data == "createdNewFile" or data == "error":
+       return jsonify(500) 
+    else:
+        return jsonify(data)
+
+@app.route('/addtodo' ,methods = ["POST"])
+def addTodo():
+    
+    newData = request.get_json()
+    # adding TODO on database
+    todo.add_data(newData)
+    return 200
+    # 200 - ok <-response
+    # 500 - error -> internal server error
+
+@app.route('/updatetodo' ,methods = ["POST"])
+def updateTodo():
+    
+    newData = request.get_json()
+    # updating TODO on database
+    todo.update_data(newData)
+    return 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',
+    app.run( # host='0.0.0.0',
             debug=True,
             port=1234,
             extra_files=[
-            "templates/",  # watch all template files
-            "static/"      # watch all static files (CSS, JS)
+            "templates/",  
+            "static/"      
         ])
---
+++ modules/todo.py
@@ -0,0 +1,42 @@
+import json
+
+def get_data():
+    try:   
+        with open("data/tosdo.json",'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError as f:
+        print(f)
+        with open("data/tosdo.json","w") as file:
+            json.dump([], file, indent=4)
+        return "createdNewFile"
+    except Exception as e:
+        print(e)
+        return "error"
+    
+def add_data(newdata):
+    with open("data/todo.json",'r') as file:
+        data = json.load(file)
+    
+    data.append(newdata)
+
+    with open("data/todo.json",'w') as file:
+        data = json.dump(data, file, indent=4)
+    
+    return
+
+def update_data(updateddata):
+    with open("data/todo.json",'r') as file:
+        datas = json.load(file)
+    
+    jsonData = []
+
+    for data in datas:
+        if data['id'] != updateddata['id']:
+            jsonData.append(data)
+            continue
+        jsonData.append(updateddata)
+    
+    with open("data/todo.json",'w') as file:
+        data = json.dump(jsonData, file, indent=4)
+
